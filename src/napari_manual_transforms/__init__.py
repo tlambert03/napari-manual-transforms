651211fb798b1a4ed0a710ff40f26fd00db0baa2
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2024-11-23 18:18:41 +0100

    add deprecation warning on importing previous name 'RotationWidget'

--- src/napari_manual_transforms/__init__.py
+++ src/napari_manual_transforms/__init__.py
@@ -9,3 +9,17 @@ except metadata.PackageNotFoundError:
 from ._widget import TransformationWidget
 
 __all__ = ["TransformationWidget"]
+
+
+def __getattr__(name: str) -> object:
+    if name == "RotationWidget":
+        import warnings
+
+        warnings.warn(
+            "'RotationWidget' has been renamed to 'TransformationWidget'.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return TransformationWidget
+
+    raise AttributeError(f"module {__name__} has no attribute {name}")
